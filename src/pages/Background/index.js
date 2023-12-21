937d7ca5985f1b9bca478dd6eae9144264ffcfdf
Travis Lawrence <travis.lawrence12@gmail.com>
2023-12-21 11:28:06 -0800

    switch in bg

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -12,45 +12,57 @@ const convertBlobToBase64 = (blob) =>
     };
   });
 
+const fetchType = (message, sendResponse) => {
+  fetch(message.url)
+    .then((resp) => {
+      return resp.blob();
+    })
+    .then(async (d) => {
+      // you have to send a base64 string back through response
+      // cause Chrome is stupid and won't let you send actual objects
+      const blob64 = await convertBlobToBase64(d);
+      sendResponse({ blob64 });
+    });
+};
+
 let cascBytes;
 let pupBytes;
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.type === 'fetch') {
-    fetch(message.url)
-      .then((resp) => {
-        return resp.blob();
-      })
-      .then(async (d) => {
-        // you have to send a base64 string back through response
-        // cause Chrome is stupid and won't let you send actual objects
-        const blob64 = await convertBlobToBase64(d);
-        sendResponse({ blob64 });
-      });
+const loadModelsType = (sendResponse) => {
+  if (!!cascBytes && !!pupBytes) {
+    sendResponse({ cascBytes, pupBytes });
+    return;
   }
 
-  if (message.type === 'loadFaceModels') {
-    if (!!cascBytes && !!pupBytes) {
-      sendResponse({ cascBytes, pupBytes });
-      return;
+  const promise = new Promise(async (resolve) => {
+    const cascadeFetch = fetch(cascadeurl);
+    const pupilFetch = fetch(puplocurl);
+    const [cascResp, pupResp] = await Promise.all([cascadeFetch, pupilFetch]);
+
+    const cascReq = cascResp.arrayBuffer();
+    const pupReq = pupResp.arrayBuffer();
+    const [cascBuffer, pupBuffer] = await Promise.all([cascReq, pupReq]);
+
+    cascBytes = new Int8Array(cascBuffer);
+    pupBytes = new Int8Array(pupBuffer);
+    resolve({ cascBytes, pupBytes });
+  });
+
+  promise.then((resp) => {
+    sendResponse(resp);
+  });
+};
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  switch (message.type) {
+    case 'fetch': {
+      fetchType(message, sendResponse);
+      break;
     }
 
-    const promise = new Promise(async (resolve) => {
-      const cascadeFetch = fetch(cascadeurl);
-      const pupilFetch = fetch(puplocurl);
-      const [cascResp, pupResp] = await Promise.all([cascadeFetch, pupilFetch]);
-
-      const cascReq = cascResp.arrayBuffer();
-      const pupReq = pupResp.arrayBuffer();
-      const [cascBuffer, pupBuffer] = await Promise.all([cascReq, pupReq]);
-
-      cascBytes = new Int8Array(cascBuffer);
-      pupBytes = new Int8Array(pupBuffer);
-      resolve({ cascBytes, pupBytes });
-    });
-
-    promise.then((resp) => {
-      sendResponse(resp);
-    });
+    case 'loadFaceModels': {
+      loadModelsType(sendResponse);
+      break;
+    }
   }
   return true;
 });
